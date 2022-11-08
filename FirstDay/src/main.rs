6e6e23a025bdcac332bf72e4a3c02658312c941a
Evgeny Momotov (Krymmy) <evgeny.momotov@gmail.com>
2022-11-08 22:35:35 +0300

    Little change firts day |Fix bug how fly bug

--- FirstDay/src/main.rs
+++ FirstDay/src/main.rs
@@ -1,4 +1,4 @@
-extern crate rand;
+use rand;
 use rand::Rng;
 use std::{thread, time};
 
@@ -11,15 +11,31 @@ struct Point{
 fn main() {
     let width: i32 = 10;
     let height: i32 = 10;
-    let mut point = Point{x:8, y:8};
+    let mut point = Point{x:5, y:5};
     let mut gX: i32;
     let mut gY: i32;
     let millis = time::Duration::from_millis(100);
     let now = time::Instant::now();
     loop{
+        if point.x >= width{
+            point.x = width-3;
+        }
+        else if point.x < 0{
+            point.x = 3
+        }
+        if point.y >= height{
+            point.y = height-3;
+        }
+        else if point.y < 0{
+            point.y = 3
+        }
         for gX in 0..width+1{
             for gY in 0..height+1{
                 if gY == point.y && gX == point.x{
+                    if point.y >= height {
+                        print!(" * \n");
+                        continue;
+                    }
                     print!(" * ");
                     continue;
                 }
@@ -43,8 +59,8 @@ fn main() {
                 }
             }
         }
-        point.x = rand::thread_rng().gen_range(1, width-1);
-        point.y = rand::thread_rng().gen_range(1, height-1);
+        point.x += rand::thread_rng().gen_range(-1, 2);
+        point.y += rand::thread_rng().gen_range(-1,2);
         print!("{}c",27 as char);
         thread::sleep(millis);
     }

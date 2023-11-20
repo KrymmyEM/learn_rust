a09f67aa2b8970ab4425867a9d740df1b135a76b
Evgeny <86221504+KrymmyEM@users.noreply.github.com>
2023-11-20 21:20:26 +0300

    Delete learnThreads/target directory

--- learnThreads/target/debug/deps/learnThreads-aeffbe888a37bce3.d
+++
@@ -1,5 +0,0 @@
-/mnt/Files/Projects/Learn/Rust/learnThreads/target/debug/deps/learnThreads-aeffbe888a37bce3.rmeta: src/main.rs
-
-/mnt/Files/Projects/Learn/Rust/learnThreads/target/debug/deps/learnThreads-aeffbe888a37bce3.d: src/main.rs
-
-src/main.rs:
--- learnThreads/target/debug/deps/learnThreads-ea49aa06cb2fc986.d
+++
@@ -1,5 +0,0 @@
-/mnt/Files/Projects/Learn/Rust/learnThreads/target/debug/deps/learnThreads-ea49aa06cb2fc986.rmeta: src/main.rs
-
-/mnt/Files/Projects/Learn/Rust/learnThreads/target/debug/deps/learnThreads-ea49aa06cb2fc986.d: src/main.rs
-
-src/main.rs:
